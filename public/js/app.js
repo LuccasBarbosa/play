a3acf6cf91062f7c3b345592e25ff3a206550bc6
Luccas Barbosa <eu@luccasbarbosa.com.br>
2021-07-19 13:24:07 -0300

    static

--- public/js/app.js
+++ public/js/app.js
@@ -36530,8 +36530,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /Library/WebServer/Documents/playNtu/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Library/WebServer/Documents/playNtu/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Library/WebServer/Documents/Sites/play/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Library/WebServer/Documents/Sites/play/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
